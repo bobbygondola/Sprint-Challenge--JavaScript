02819825a427bf4e0bb9be0d2cd762b611b8e7de
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-03-27 13:19:00 -0400

    yuh

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -87,7 +87,6 @@ Once you have the new array created, log the result. */
 const universities = [];
 for (let i=0; i < graduates.length; i++)
 universities.push(graduates[i].university)
-universities.sort();
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -133,7 +132,7 @@ const zooAnimals = [
 
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
-***********************************************************************DONE****************************************************************************/*/
+***********************************************************************DONE****************************************************************************/
 
 const displayNames = [];
 zooAnimals.forEach(element => displayNames.push(`${element.animal_name} ${element.scientific_name}`));
@@ -165,7 +164,7 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = zooAnimals.reduce((accumulator, current) => {
+const populationTotal = zooAnimals.reduce((acccumulator, current) => {
 return acccumulator + current.population;
 }, 0);
 console.log(populationTotal);
